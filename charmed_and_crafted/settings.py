708c56ab09096fade0f1beaaab4c4d7cbd2dd00d
Volnei Resena Junior <volnei.resena.j@hotmail.com>
2024-10-31 15:17:33 +0000

    Add home background and style it

--- charmed_and_crafted/settings.py
+++ charmed_and_crafted/settings.py
@@ -177,7 +177,7 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-
+print(MEDIA_ROOT)
 
 # if 'USE_AWS' in os.environ:
 #     # Cache control
