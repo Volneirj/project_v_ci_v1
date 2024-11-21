9c6a1b42491fc8a56ab07e277ad6aaf57219a40b
Volnei Resena Junior <volnei.resena.j@hotmail.com>
2024-11-21 18:26:26 +0000

    Config  email for registration link

--- charmed_and_crafted/settings.py
+++ charmed_and_crafted/settings.py
@@ -220,6 +220,18 @@ STRIPE_CURRENCY = 'usd'
 STRIPE_PUBLIC_KEY = config('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = config('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = config('STRIPE_WH_SECRET', '')
-DEFAULT_FROM_EMAIL = 'charmedandcrafted@example.com'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+if 'DEVELOPMENT' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = 'charmedandcrafted@example.com'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = config('EMAIL_HOST_USER')
+
 
