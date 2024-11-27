801089b00549c03ce22258b0277c0c9df1a9e7f8
Volnei Resena Junior <volnei.resena.j@hotmail.com>
2024-11-27 19:10:48 +0000

    Fix input shop bag
    Fixed the bug when click to add an item on shopping bag and visualy change all inputs

--- products/forms.py
+++ products/forms.py
@@ -23,6 +23,10 @@ class ProductForm(forms.ModelForm):
         categories = Category.objects.all()
         friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
 
+        # Remove the 'sizes' field from the form
+        if 'has_sizes' in self.fields:
+            del self.fields['has_sizes']
+
         self.fields['category'].choices = friendly_names
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'border-black rounded-0'
