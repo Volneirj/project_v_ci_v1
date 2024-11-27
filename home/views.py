03e28a558229f4a072efc2091f13d88c528937a3
Volnei Resena Junior <volnei.resena.j@hotmail.com>
2024-11-27 14:50:39 +0000

    Review home app files
    Added a few docstrings on created files to remove lynt warnings

--- home/views.py
+++ home/views.py
@@ -1,3 +1,4 @@
+"""Home views setup"""
 from django.shortcuts import render
 
 # Create your views here.
@@ -7,4 +8,3 @@ def index(request):
     A view to return the index page
     """
     return render(request, 'home/index.html')
-
