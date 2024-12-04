b31c52e8f9b256695ba44452219d07a44aea81bd
Volnei Resena Junior <volnei.resena.j@hotmail.com>
2024-12-04 15:48:24 +0000

    Remove django countries from settings

--- charmed_and_crafted/settings.py
+++ charmed_and_crafted/settings.py
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
-    'django_countries',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
